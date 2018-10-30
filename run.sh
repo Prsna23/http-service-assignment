f43bb7f83b414a3ef8e78ed7b8aa203ec7e0a7f9
prsna23 <prasannajagathesan@indix.com>
2018-10-30 17:05:12 +0530

    Added tag to docker image

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 sbt clean test assembly
-docker build . -t prsna23/httphelloworld
+docker build . -t prsna23/httphelloworld:$GO_PIPELINE_LABEL
 docker login -u "$USERNAME" -p "$PASSWORD"
-docker push prsna23/httphelloworld
+docker push prsna23/httphelloworld:$GO_PIPELINE_LABEL
 
